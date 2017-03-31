967089d5f01f5d9121b94492689ecc5ace75e6b4
Vilderr <admin@mulle.ru>
2017-03-31 11:42:59 +0300

    Modern main menu

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -29,7 +29,7 @@ AppAsset::register($this);
     <div class="wrapper">
         <div class="header">
             <div class="logo">
-                <?= Html::a('BaseProducts.ru', Yii::$app->homeUrl, ['id' => 'home-logo']);?>
+                <?= Html::a('BaseProducts.ru', Yii::$app->homeUrl, ['id' => 'home-logo']); ?>
             </div>
             <div class="nav">
                 <a href="<?= Url::to(['/sign/out']) ?>" class="pull-right"><i class="glyphicon glyphicon-log-out"></i>
@@ -43,9 +43,21 @@ AppAsset::register($this);
                         'id'    => 'admin-menu',
                         'items' => [
                             [
+                                'label' => 'Каталог товаров',
+                                'url'   => Url::to(['/reference', 'type' => 'catalog']),
+                            ],
+                            [
+                                'label' => 'Свойства товаров',
+                                'url'   => Url::to(['/reference', 'type' => 'props']),
+                            ],
+                            [
+                                'label' => 'Импорт товаров',
+                                'url'   => Url::to(['/reference/import', 'type' => 'catalog', 'reference_id' => 3]),
+                            ],
+                            [
                                 'label' => 'Типы справочников',
                                 'url'   => Url::toRoute('/reference-type'),
-                            ]
+                            ],
                         ],
                     ]
                 ); ?>

7fb63c3adeb678a03f80b96ec19032bbb1d4b9bc
Vilderr <admin@mulle.ru>
2017-04-05 15:35:25 +0300

    BugFix: distribution parser

---
+++ app/models/file/File.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VILDERR
+ * Date: 05.04.17
+ * Time: 14:24
+ */
+
+namespace app\models\file;
+
+
+class File
+{
+
+}
---
+++ app/models/file/TempFile.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VILDERR
+ * Date: 05.04.17
+ * Time: 15:01
+ */
+
+namespace app\models\file;
+
+
+class TempFile
+{
+
+}

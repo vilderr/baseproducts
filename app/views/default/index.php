bafb0286c902621e98a3b38fa9a2f6c896b432e4
Vilderr <admin@mulle.ru>
2017-04-06 23:07:20 +0300

    fvefv

--- app/views/default/index.php
+++ app/views/default/index.php
@@ -21,6 +21,36 @@ use app\core\http\HttpRequest;
 
 //echo '<pre>'; print_r($loaded); echo '</pre>';
 
-$arFile = File::makeArray('https://blackstarshop.ru/image/catalog2/women/LA1817-500/LA1817-500d.png');
-echo '<pre>'; print_r($arFile); echo '</pre>';
+//$arFile = File::makeArray('https://blackstarshop.ru/image/catalog2/women/LA1817-500/LA1817-500d.png');
+//echo '<pre>'; print_r($arFile); echo '</pre>';
+$file = Yii::$app->getBasePath().'/upload/image.png';
+$url = 'https://blackstarshop.ru/image/catalog2/women/LA1817-500/LA1817-500d.png';
+curl_download($url, $file);
+
+function curl_download($url, $file)
+{
+    // открываем файл, на сервере, на запись
+    $dest_file = @fopen($file, "w");
+
+    // открываем cURL-сессию
+    $resource = curl_init();
+
+    // устанавливаем опцию удаленного файла
+    curl_setopt($resource, CURLOPT_URL, $url);
+
+    // устанавливаем место на сервере, куда будет скопирован удаленной файл
+    curl_setopt($resource, CURLOPT_FILE, $dest_file);
+
+    // заголовки нам не нужны
+    curl_setopt($resource, CURLOPT_HEADER, 0);
+
+    // выполняем операцию
+    curl_exec($resource);
+
+    // закрываем cURL-сессию
+    curl_close($resource);
+
+    // закрываем файл
+    fclose($dest_file);
+}
 

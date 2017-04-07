6fe6c6aa75a5d263e455e64bf7f3da86a2945058
Vilderr <admin@mulle.ru>
2017-04-07 11:29:20 +0300

    Wfv

--- app/views/default/index.php
+++ app/views/default/index.php
@@ -26,7 +26,7 @@ use app\models\reference\ReferenceElement;
 $from = "http://image01.bonprix.ru/bonprixbilder/429x60770/1459330988/15045961-pJYZXWVt.jpg";
 $to = Yii::$app->getBasePath().'/upload';
 exec("cd $to && /usr/bin/wget $from > /dev/null &", $result, $error);
-echo $result;
+echo '<pre>'; print_r($result); echo '</pre>';
 
 
 /*
